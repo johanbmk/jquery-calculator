320a3242cc0a195b9d155b638c86de5c98f7afda
Johan Brattemark <johan.brattemark@gmail.com>
2017-05-05 11:59:04 -0700

    Display special chars for multiplication and division

--- js/index.js
+++ js/index.js
@@ -1,25 +1,56 @@
 function keyAction(event) {
   var buttonText = event.srcElement.innerText;
-  if      (buttonText === 'C') { buffer = '0' }
-  else if (buttonText === '÷') { buffer += '/' }
-  else if (buttonText === 'x') { buffer += '*' }
-  else if (buttonText === '-') { buffer += '-' }
-  else if (buttonText === '+') { buffer += '+' }
-  else if (buttonText === '=') { buffer = eval(buffer) }
-  else {
+  if (buttonText === 'C') {
+    error = false;
+    expression = '0';
+    display = '0';
+  } else if (buttonText === '÷' && !error) {
+    expression += '/';
+    display += '÷';
+  } else if (buttonText === 'x' && !error) {
+    expression += '*';
+    display += 'x';
+  } else if (buttonText === '-' && !error) {
+    expression += '-';
+    display += '-';
+  } else if (buttonText === '+' && !error) {
+    expression += '+';
+    display += '+';
+  } else if (buttonText === '=' && !error) {
+    expression = checkAndEval(expression);
+    display = expression;
+  } else if (!error) {
     // a number key was clicked
-    if (buffer === '0') {
-      buffer = '';
+    if (expression === '0') {
+      expression = '';
+      display = '';
     }
-    buffer += buttonText;
+    expression += buttonText;
+    display += buttonText;
   }
-  $('#screen').text(buffer);
+  $('#screen').text(display);
 }
 
-var buffer = '0';
+function checkAndEval(exp) {
+  var result;
+  try {
+    result = eval(exp);
+  }
+  catch(err) {
+    error = true;
+  }
+  if (error) {
+    result = 'Error';
+  }
+  return result;
+}
+
+var error = false;
+var expression = '0';
+var display = '0';
 
 $(document).ready(() => {
-  $('#screen').text(buffer);
+  $('#screen').text(display);
   $('.buttons').children().click(() => {
     keyAction(event);
   });
